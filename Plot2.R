450733fd41190fdd552099db1e544f379bc9af69
Lray12321 <93883650+Lray12321@users.noreply.github.com>
2021-12-05 10:19:28 -0500

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -9,13 +9,13 @@ SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.
 NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
 
 NEI[, Emissions := lapply(.SD, as.numeric), .SDcols = c("Emissions")]
-totalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
+TotalNEI <- NEI[fips=='24510', lapply(.SD, sum, na.rm = TRUE)
                 , .SDcols = c("Emissions")
                 , by = year]
 
 png(filename='plot2.png')
 
-barplot(totalNEI[, Emissions]
-        , names = totalNEI[, year]
+barplot(TotalNEI[, Emissions]
+        , names = TotalNEI[, year]
         , xlab = "Years", ylab = "Emissions"
         , main = "Emissions over the Years (Baltimore City)")
