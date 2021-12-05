892c49ea7d2a2e6d2db901bb289c33b4450d778f
Lray12321 <93883650+Lray12321@users.noreply.github.com>
2021-12-05 10:20:18 -0500

    Create Plot3.R

---
+++ Plot3.R
@@ -0,0 +1,22 @@
+library(data.table)
+library(ggplot2)
+path <- getwd()
+download.file(url = "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+              , destfile = paste(path, "dataFiles.zip", sep = "/"))
+unzip(zipfile = "dataFiles.zip")
+
+SCC <- data.table::as.data.table(x = readRDS(file = "Source_Classification_Code.rds"))
+NEI <- data.table::as.data.table(x = readRDS(file = "summarySCC_PM25.rds"))
+
+BaltNEI <- NEI[fips=="24510",]
+
+png("plot3.png")
+
+ggplot(BaltNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source"))
+
+dev.off()
